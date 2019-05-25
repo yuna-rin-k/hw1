d7aef652f3c5821e1f11136edcb48c1c979054ee
yuna <g17922ky@gm.tsuda.ac.jp>
2019-05-25 17:55:06 +0900

    ver2

--- WinHazWordz.java
+++ WinHazWordz.java
@@ -49,62 +49,7 @@ public class WinHazWordz {
 		}
 		return point;
 	}
-	/*
-	public int search (String s) {
-		System.out.println("key:"+key);
-		System.out.println("s:"+s);
-
-		for (int i = 0; i < key.length(); ++i) {arrayKey.add(key.charAt(i)); }
-		int n = key.length();
-		int m = s.length();
-		int[] char_key = new int[n];
-		int[] char_s = new int[m];
-		int[][] dp = new int[n+1][m+1];
-		int[][] dpP = new int[n+1][m+1];
-
-		for (int i = 0; i < n; ++i) { char_key[i] = key.charAt(i);}
-		for (int i = 0; i < m; ++i) { char_s[i] = s.charAt(i);}
-		//Arrays.fill (dp, 0);
-		//Arrays.fill (dpP,0);
-		for (int i = 0; i <= n; ++i ){
-			for (int j = 0; j <= m; ++j ){
-				dp[i][j] = 0;
-				dpP[i][j] = 0;
-			}
-		}
-		int highP = 0;
-
-		for (int i = 0; i < n; ++i){
-			for (int j = 0; j < m; ++j) {
-				if (!arrayKey.contains((char)char_s[j])) {
-				 return -1;
-				} else {
-					int index = arrayKey.indexOf((char)char_s[j]);
-					arrayKey.remove(index);
-				}
-				if (char_key[i] == char_s[j]) {
-					System.out.println("TEST");
-					dp[i+1][j+1] = dp[i][j]+1;
-						if (oneP.contains(char_key[i])) dpP[i+1][j+1] = dpP[i][j]+1;
-						else if (twoP.contains(char_key[i])) dpP[i+1][j+1] = dpP[i][j]+2;
-						else if (threeP.contains(char_key[i])) dpP[i+1][j+1] = dpP[i][j]+3;
-				} else {
-					//System.out.println("TEST2");
-					if (dp[i][j+1] > dp[i+1][j]){
-						dp[i+1][j+1] = dp[i][j+1];
-						dpP[i+1][j+1] = dpP[i][j+1];
-					} else {
-						dp[i+1][j+1] = dp[i+1][j];
-						dpP[i+1][j+1] = dpP[i+1][j];
-					}
-					//dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
-				}
-				System.out.println(dp[i+1][j+1]);
-
-			}
-		}
-		return dpP[n][m];
-	}*/
+	
 
 	public String runSearch () {
 		String highPStr = "";
